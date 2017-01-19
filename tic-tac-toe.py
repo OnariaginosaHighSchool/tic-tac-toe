b50b57bcecb9b5894742ce734621913d14ec7833
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-19 10:49:27 -0800

    fixed switch

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,20 +6,80 @@ import tkMessageBox
 from PIL import Image, ImageTk
 
 
+def switch1(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
 
-def switch():
-    x = 0
-    if x % 2 == 0:
-        print'hey'
-        x += 1
-    else:
-        print'no'
-        x+=1
+    label1 = Label(image=photo)
+    label1.image = photo # keep a reference!
+    label1.grid(row=7, column =0 )
     
-    
-def turn():
-    
-    label1.config(text=switch())
+def switch2(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label2 = Label(image=photo)
+    label2.image = photo # keep a reference!
+    label2.grid(row=30, column =0 )
+def switch3(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label3 = Label(image=photo)
+    label3.image = photo # keep a reference!
+    label3.grid(row=70, column =0 )
+def switch4(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label4 = Label(image=photo)
+    label4.image = photo # keep a reference!
+    label4.grid(row=7, column =1 )
+def switch5(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label5 = Label(image=photo)
+    label5.image = photo # keep a reference!
+    label5.grid(row=30, column =1 )
+def switch6(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label6 = Label(image=photo)
+    label6.image = photo # keep a reference!
+    label6.grid(row=70, column =1 )
+def switch7(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label7 = Label(image=photo)
+    label7.image = photo # keep a reference!
+    label7.grid(row=7, column =2 )
+def switch8(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label8 = Label(image=photo)
+    label8.image = photo # keep a reference!
+    label8.grid(row=30, column =2 )
+def switch9(event):
+    X = Image.open("X.png")
+    X = X.resize((100,100))
+    photo = ImageTk.PhotoImage(X)
+
+    label9 = Label(image=photo)
+    label9.image = photo # keep a reference!
+    label9.grid(row=70, column =2 )
+
 
 def buttonpress():
     entrytxt = entry1.get()
@@ -32,16 +92,13 @@ def openfileR():
     for line in f:
 
         name = line[0:-1]
-        listbox1.insert(END, name)
+
     f.close()
 
 def openfileW():
     f = open("Readme.txt", 'w')
-    names = listbox1.get(0,END)
     for i in names:    
         f.write(i + "\n") 
-    
-    f.close()
     
 
 root = Tk() #gives us a blank canvas object to work with
@@ -53,7 +110,6 @@ root.title = ("GUI Program")
 label1 = Label(root, text ="Player 1 Go First", bg="pink", anchor=W)
 label1.grid(row=0, column=0, sticky=EW, columnspan=2)
 
-turn()
 
 
 
@@ -62,51 +118,56 @@ image = Image.open("blank.png")
 image = image.resize((100,100))
 photo = ImageTk.PhotoImage(image)
     
+
+    
+    
 label1 = Label(image=photo)
 label1.image = photo # keep a reference!
 label1.grid(row=7, column =0 )
-label1.bind("<Button-3>",switch )
+label1.bind("<Button-3>", switch1)
 
 label2 = Label(image=photo)
 label2.image = photo # keep a reference!
 label2.grid(row=30, column =0 )
-label2.bind("<Button-3>",switch )
+label2.bind("<Button-3>", switch2)
 
 label3 = Label(image=photo)
 label3.image = photo # keep a reference!
 label3.grid(row=70, column =0 )
-label3.bind("<Button-3>",switch )
+label3.bind("<Button-3>", switch3)
 
 label4 = Label(image=photo)
 label4.image = photo # keep a reference!
 label4.grid(row=7, column =1 )
-label4.bind("<Button-3>",switch )
+label4.bind("<Button-3>", switch4 )
 
 label5 = Label(image=photo)
 label5.image = photo # keep a reference!
 label5.grid(row=30, column =1 )
-label5.bind("<Button-3>",switch )
+label5.bind("<Button-3>", switch5)
 
 label6 = Label(image=photo)
 label6.image = photo # keep a reference!
 label6.grid(row=70, column =1 )
-label6.bind("<Button-3>",switch )
+label6.bind("<Button-3>", switch6 )
 
 label7 = Label(image=photo)
 label7.image = photo # keep a reference!
 label7.grid(row=7, column =2 )
-label7.bind("<Button-3>",switch)
+label7.bind("<Button-3>", switch7)
 
 label8 = Label(image=photo)
 label8.image = photo # keep a reference!
 label8.grid(row=30, column =2 )
-label8.bind("<Button-3>",switch )
+label8.bind("<Button-3>", switch8 )
 
 label9 = Label(image=photo)
 label9.image = photo # keep a reference!
 label9.grid(row=70, column =2 )
-label9.bind("<Button-3>",switch )
+label9.bind("<Button-3>", switch9)
 
+def callback(event):
+    print "clicked at", event.x, event.y
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
